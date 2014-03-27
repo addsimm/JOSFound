364ebf10e32a9cf53927742d2b7de29ba01d3980
adamsimon <adamfsimon@gmail.com>
2014-03-27 10:28:41 -0700

    Uploaded setting files.

--- JOSFound/settings/base.py
+++ JOSFound/settings/base.py
@@ -1,1 +1,80 @@
+# settings/base.py
+
+# per two scoops of Django pg. 46
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+def get_env_variable(var_name):
+	"""Get the enviroment variable or return exception."""
+	try:
+		return os.environ(var_name)
+	except KeyError:
+		error_msg = "Set the %s environment variable" % var_name
+		raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_env_variable("SECRET_KEY")
+
+
+
+
+
 __author__ = 'adamsimon'
+
+"""
+Django settings for JOSFound project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'JOSHome',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'JOSFound.urls'
+
+WSGI_APPLICATION = 'JOSFound.wsgi.application'
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
--- JOSFound/settings/local.py
+++ JOSFound/settings/local.py
@@ -1,1 +1,27 @@
+
+# settings/local.py
+
+from .base import *
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 __author__ = 'adamsimon'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
